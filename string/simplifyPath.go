f2bbc428f86e589995714d5a757d7d92cd3127c3
zlbonly <zhuliubaoonly@163.com>
2020-04-22 00:35:25 +0800

    feat: leetcode 1、采用栈规划目录路径

---
+++ string/simplifyPath.go
@@ -0,0 +1,76 @@
+package main
+
+import (
+	"fmt"
+	"strings"
+)
+
+/*以 Unix 风格给出一个文件的绝对路径，你需要简化它。或者换句话说，将其转换为规范路径。
+在 Unix 风格的文件系统中，一个点（.）表示当前目录本身；此外，
+两个点 （..） 表示将目录切换到上一级（指向父目录）；两者都可以是复杂相对路径的组成部分。
+更多信息请参阅：Linux / Unix中的绝对路径 vs 相对路径
+请注意，返回的规范路径必须始终以斜杠 / 开头，并且两个目录名之间必须只有一个斜杠 /。
+最后一个目录名（如果存在）不能以 / 结尾。此外，规范路径必须是表示绝对路径的最短字符串。
+
+示例 1：
+
+输入："/home/"
+输出："/home"
+解释：注意，最后一个目录名后面没有斜杠。
+示例 2：
+
+输入："/../"
+输出："/"
+解释：从根目录向上一级是不可行的，因为根是你可以到达的最高级。
+示例 3：
+
+输入："/home//foo/"
+输出："/home/foo"
+解释：在规范路径中，多个连续斜杠需要用一个斜杠替换。
+示例 4：
+
+输入："/a/./b/../../c/"
+输出："/c"
+示例 5：
+
+输入："/a/../../b/../c//.//"
+输出："/c"
+示例 6：
+
+输入："/a//b////c/d//././/.."
+输出："/a/b/c"*/
+
+func main() {
+	path := "/a//b////c/d//././/.."
+	result := simplifyPath(path)
+	fmt.Println("规范后路径path:", result)
+}
+
+/**
+解题思路
+用stack模拟目录的进退。
+
+用split分割一下。
+为空或者为"."，则skip。
+".."的情况，如果stack里面有值，则Pop栈顶。
+其余则Push入栈。
+最后Join一下。
+*/
+func simplifyPath(path string) string {
+	buf := strings.Split(path, "/")
+	var stack []string
+
+	for i := 0; i < len(buf); i++ {
+		if buf[i] == "" || buf[i] == "." {
+			continue
+		}
+		if buf[i] == ".." {
+			if len(stack) > 0 {
+				stack = stack[0 : len(stack)-1]
+			}
+		} else {
+			stack = append(stack, buf[i])
+		}
+	}
+	return "/" + strings.Join(stack, "/")
+}
