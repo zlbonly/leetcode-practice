acd8409f42e0c60fcf7ffa9041e9d4768cb19bc4
zlbonly <zhuliubaoonly@163.com>
2020-04-29 01:45:32 +0800

    feat: leetcode 1、链表分割， 2、链表排序 3、链表相邻节点交换

--- linkedList/linkNode.go
+++ linkedList/linkNode.go
@@ -172,3 +172,143 @@ func (this *List) reverseStack() *Node {
 	}
 	return res
 }
+
+/**
+编写程序以 x 为基准分割链表，使得所有小于 x 的节点排在大于或等于 x 的节点之前。如果链表中包含 x，x 只需出现在小于 x 的元素之后(如下所示)。分割元素 x 只需处于“右半部分”即可，其不需要被置于左右两部分之间。
+
+示例:
+
+输入: head = 3->5->8->5->10->2->1, x = 5
+输出: 3->1->2->10->5->5->8
+
+解题思路： 双指针法
+
+*/
+func (this *List) partitionNode(partition Object) *Node {
+
+	cur := this.headNode
+	var res *Node
+	var smaller *Node
+	var bigger *Node
+	for cur != nil {
+		if cur.Data.(int) < partition.(int) {
+			smaller = &Node{cur.Data, smaller}
+		} else {
+			bigger = &Node{cur.Data, bigger}
+		}
+		cur = cur.Next
+	}
+	for true {
+		if bigger == nil {
+			break
+		}
+		res = &Node{bigger.Data, res}
+		bigger = bigger.Next
+	}
+	for true {
+		if smaller == nil {
+			break
+		}
+
+		res = &Node{smaller.Data, res}
+		smaller = smaller.Next
+	}
+
+	return res
+}
+
+/*
+
+题目：
+输入两个递增排序的链表，合并这两个链表并使新链表中的节点仍然是递增排序的。
+
+示例1：
+
+输入：1->2->4, 1->3->4
+输出：1->1->2->3->4->4
+限制：
+
+0 <= 链表长度 <= 1000
+
+解题思路： 采用双指针法
+*/
+
+func mergeTwoOrderList(l1 *Node, l2 *Node) *Node {
+	if l1 == nil {
+		return l2
+	}
+	if l2 == nil {
+		return l1
+	}
+	var head *Node
+	if l1.Data.(int) < l2.Data.(int) {
+		head = l1
+		l1 = l1.Next
+	} else {
+		head = l2
+		l2 = l2.Next
+	}
+
+	pre := head
+	for l1 != nil && l2 != nil {
+		if l1.Data.(int) < l2.Data.(int) {
+			pre.Next = l1
+			l1 = l1.Next
+		} else {
+			pre.Next = l2
+			l2 = l2.Next
+		}
+		pre = pre.Next
+	}
+
+	if l1 != nil {
+		pre.Next = l1
+	}
+	if l2 != nil {
+		pre.Next = l2
+	}
+	return head
+}
+
+/*
+给定一个链表，两两交换其中相邻的节点，并返回交换后的链表。
+你不能只是单纯的改变节点内部的值，而是需要实际的进行节点交换。
+
+示例:
+给定 1->2->3->4, 你应该返回 2->1->4->3.
+
+解题思路：
+迭代法：
+
+我们把链表分为两部分，即奇数节点为一部分，偶数节点为一部分，A 指的是交换节点中的前面的节点，B 指的是要交换节点中的后面的节点。在完成它们的交换，我们还得用 prevNode 记录 A 的前驱节点。
+
+算法：
+
+1、firstNode（即 A） 和 secondNode（即 B） 分别遍历偶数节点和奇数节点，即两步看作一步。
+2、交换两个节点：
+ firstNode.next = secondNode.next
+ secondNode.next = firstNode
+3、还需要更新 prevNode.next 指向交换后的头。
+	prevNode.next = secondNode
+4、迭代完成后得到最终的交换结果。
+
+
+*/
+func swapPairs(head *Node) *Node {
+	if head == nil {
+		return nil
+	}
+	dump := new(Node) //  # Dummy node acts as the prevNode for the head node
+	dump.Next = head
+	prev := dump
+	for head != nil {
+		cur1 := head
+		cur2 := head.Next
+		prev.Next = cur2
+		cur1.Next = cur2.Next
+		cur2.Next = cur1
+		prev = cur1
+		head = cur1.Next
+	}
+	return dump.Next
+}
--- linkedList/testLinkedList.go
+++ linkedList/testLinkedList.go
@@ -1,28 +1,36 @@
 package main
 
-import (
-	"fmt"
-)
+import "fmt"
 
 func main() {
-	list := List{}
-	list.append(1)
+	/*list := List{}
+	list.append(9)
 	list.append(2)
 	list.append(3)
+	list.append(10)
+	list.append(5)
+	list.append(5)
+	list.append(8)
+	list.append(1)*/
+
 	/*list.append("a")
 	list.append("b")
 	list.append("c")*/
 
-	fmt.Printf("list链表的长度：%d\n", list.length())
+	/*fmt.Printf("list链表的长度：%d\n", list.length())
 	fmt.Printf("list链表的数值")
 	list.showList()
 	fmt.Println()
-	fmt.Println()
+	fmt.Println()*/
 
-	revers := list.reverseStack()
+	// 链表反转
+	//revers := list.reverseStack()
+	//list1 := List{revers}
 
-	list1 := List{revers}
-	list1.showList()
+	// 链表分割
+	/*partitionNode := list.partitionNode(5)
+	list1 := List{partitionNode}
+	list1.showList()*/
 
 	/*
 		fmt.Print("当前链表头部插入元素")
@@ -30,4 +38,55 @@ func main() {
 		fmt.Printf("list链表的数值")
 		list.showList()
 	*/
+
+	/*
+		合并两个有序链表
+
+		list := List{}
+		list.append(1)
+		list.append(5)
+		list.append(6)
+		fmt.Printf("list1链表的数值")
+		list.showList()
+
+		fmt.Println()
+
+		list2 := List{}
+		list2.append(4)
+		list2.append(9)
+		list2.append(10)
+		list2.append(11)
+
+		fmt.Printf("list2链表的数值")
+		list2.showList()
+
+		fmt.Println()
+		fmt.Printf("合并后list3链表的数值")
+
+		 node := mergeTwoOrderList(list.headNode,list2.headNode)
+
+		list3 := List{node}
+		list3.showList()
+	*/
+
+	// 链表相邻节点交换
+
+	list := List{}
+	list.append(1)
+	list.append(2)
+	list.append(3)
+	list.append(4)
+	list.append(5)
+	list.append(6)
+
+	fmt.Printf("交换前的链表")
+	list.showList()
+	fmt.Println()
+
+	fmt.Printf("交换后的链表")
+
+	node := swapPairs(list.headNode)
+	list2 := List{node}
+	list2.showList()
+
 }
