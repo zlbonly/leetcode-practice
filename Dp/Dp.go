eaa061bfd1fd8c0628c09457874e908d7164b9ac
zlbonly <zhuliubaoonly@163.com>
2020-06-13 22:40:35 +0800

    feat: 动态规划之 - 子序列最大和

--- Dp/Dp.go
+++ Dp/Dp.go
@@ -4,7 +4,11 @@ import "fmt"
 
 func main() {
 	//1、爬楼梯
-	climbStairs(6)
+	//climbStairs(6)
+	arr := []int{-2, 1, -3, 4, -1, 2, 1, -5, 4}
+	a := maxSubArray(arr)
+	fmt.Println(a)
+
 }
 
 /**
@@ -38,4 +42,41 @@ func climbStairs(n int) {
 		stairs[i] = stairs[i-1] + stairs[i-2]
 	}
 	fmt.Println(stairs[n-1])
+}
+
+/**
+	题目描述：
+给定一个整数数组 nums ，找到一个具有最大和的连续子数组（子数组最少包含一个元素），返回其最大和。
+示例:
+输入: [-2,1,-3,4,-1,2,1,-5,4],
+输出: 6
+解释: 连续子数组 [4,-1,2,1] 的和最大，为 6。
+解决方案：动态规划
+于是可以写出这样的动态规划转移方程：
+f(i) = max { f(i - 1) + a[i], a[i]}
+
+算饭分析：
+复杂度
+时间复杂度：O(n)，其中 n 为 nums 数组的长度。我们只需要遍历一遍数组即可求得答案。
+空间复杂度：O(1)。我们只需要常数空间存放若干变量。
+*/
+func maxSubArray(nums []int) int {
+	if len(nums) == 0 {
+		return 0
+	}
+	dp := make([]int, len(nums))
+	dp[0] = nums[0]
+	res := dp[0]
+	for i := 1; i < len(nums); i++ {
+		dp[i] = max(nums[i], dp[i-1]+nums[i])
+		res = max(res, dp[i])
+	}
+	return res
+}
+
+func max(a int, b int) int {
+	if a > b {
+		return a
+	}
+	return b
 }
