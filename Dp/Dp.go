2930143fd98e93f2ce22cea1927f48a90142c4a7
zlbonly <zhuliubaoonly@163.com>
2020-06-19 01:15:08 +0800

    feat: 售卖股票的最大时机

--- Dp/Dp.go
+++ Dp/Dp.go
@@ -3,10 +3,17 @@ package main
 import "fmt"
 
 func main() {
-	//1、爬楼梯
-	//climbStairs(6)
+	/*1、爬楼梯
+	climbStairs(6)*/
+
+	/* 2 最大子序列和
 	arr := []int{-2, 1, -3, 4, -1, 2, 1, -5, 4}
 	a := maxSubArray(arr)
+	fmt.Println(a)*/
+
+	// 3 买卖股票的最佳时机
+	arr := []int{7, 1, 5, 3, 6, 4}
+	a := maxProfit(arr)
 	fmt.Println(a)
 
 }
@@ -72,6 +79,38 @@ func maxSubArray(nums []int) int {
 		res = max(res, dp[i])
 	}
 	return res
+}
+
+/**
+3、买卖股票的最佳时机
+给定一个数组，它的第 i 个元素是一支给定股票第 i 天的价格。
+如果你最多只允许完成一笔交易（即买入和卖出一支股票一次），设计一个算法来计算你所能获取的最大利润。
+注意：你不能在买入股票前卖出股票。
+
+输入: [7,1,5,3,6,4]
+输出: 5
+解释: 在第 2 天（股票价格 = 1）的时候买入，在第 5 天（股票价格 = 6）的时候卖出，最大利润 = 6-1 = 5 。
+     注意利润不能是 7-1 = 6, 因为卖出价格需要大于买入价格；同时，你不能在买入前卖出股票。
+
+解决方法： 动态规划
+1、维护2个变量：截至当前历史最低价，截止当前最大利差，迭代一遍求当天利差，与最大利差相比求较大值
+
+复杂度分析
+时间复杂度：O(n)，只需要遍历一次。
+空间复杂度：O(1)，只使用了常数个变量。
+*/
+func maxProfit(prices []int) int {
+	minPrice := prices[0]
+	maxProfit := 0
+	for i := 1; i < len(prices); i++ {
+
+		if minPrice > prices[i] {
+			minPrice = prices[i]
+		} else if prices[i]-minPrice > maxProfit {
+			maxProfit = prices[i] - minPrice
+		}
+	}
+	return maxProfit
 }
 
 func max(a int, b int) int {
