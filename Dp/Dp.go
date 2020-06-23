674c9011b86be09273353b8dfb3348944c24fcaf
zlbonly <zhuliubaoonly@163.com>
2020-06-24 01:15:21 +0800

    feat: 售卖股票的最大时机2

--- Dp/Dp.go
+++ Dp/Dp.go
@@ -11,10 +11,15 @@ func main() {
 	a := maxSubArray(arr)
 	fmt.Println(a)*/
 
-	// 3 买卖股票的最佳时机
-	arr := []int{7, 1, 5, 3, 6, 4}
-	a := maxProfit(arr)
-	fmt.Println(a)
+	// 3 买卖股票的最佳时机(1)
+	/*arr := []int{7, 1, 5, 3, 6, 4}
+	a := maxProfit(arr)*/
+
+	//arr := []int{7, 1, 5, 3, 6, 4}
+	arr := []int{1, 2, 3, 4, 5}
+
+	b := maxProfitTwo(arr)
+	fmt.Println(b)
 
 }
 
@@ -108,6 +113,33 @@ func maxProfit(prices []int) int {
 			minPrice = prices[i]
 		} else if prices[i]-minPrice > maxProfit {
 			maxProfit = prices[i] - minPrice
+		}
+	}
+	return maxProfit
+}
+
+/**
+	4、买卖股票（2）
+	题目描述：
+		给定一个数组，它的第 i 个元素是一支给定股票第 i 天的价格。
+		设计一个算法来计算你所能获取的最大利润。你可以尽可能地完成更多的交易（多次买卖一支股票）。
+		注意：你不能同时参与多笔交易（你必须在再次购买前出售掉之前的股票）。
+
+ 	输入: [7,1,5,3,6,4]
+	输出: 7
+	解释: 在第 2 天（股票价格 = 1）的时候买入，在第 3 天（股票价格 = 5）的时候卖出, 这笔交易所能获得利润 = 5-1 = 4 。
+     随后，在第 4 天（股票价格 = 3）的时候买入，在第 5 天（股票价格 = 6）的时候卖出, 这笔交易所能获得利润 = 6-3 = 3
+
+	解题方案：
+	这个题用暴力法就有点太复杂了，得稍微转换一下思路，最困扰我们的地方就是当1，2，3，4，5这类型的情况，我们不知道是需要卖出再买还是等最高价卖出，
+	但是换个思路想一下，5-1等于（2-1）+（3-2）+（4-3）+（5-4），也就是说这种情况只要后面数字大于前面数字，那就叠加利润，
+、	最后一定是最大利润，如果中间有一个数是小的，那么直接跳过即可，相当于分成了几个这样的数列。
+*/
+func maxProfitTwo(prices []int) int {
+	maxProfit := 0
+	for i := 1; i < len(prices); i++ {
+		if prices[i] > prices[i-1] {
+			maxProfit += prices[i] - prices[i-1]
 		}
 	}
 	return maxProfit
