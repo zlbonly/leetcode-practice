8db3c5c0dddd06f7972a46f3c18ca354fc98796c
[zhuliubao] <zhuliubao@qie.tv>
2021-05-08 18:37:14 +0800

    fix: 搜索二维矩阵

--- num/twoNum.go
+++ num/twoNum.go
@@ -66,6 +66,72 @@ func twoNum1(nums []int, target int) []int {
 	return nil
 }
 
+/**
+21. 搜索二维矩阵I
+编写一个高效的算法来判断 m x n 矩阵中，是否存在一个目标值。该矩阵具有如下特性：
+每行中的整数从左到右按升序排列。每行的第一个整数大于前一行的最后一个整数。
+
+输入：matrix = [[1,3,5,7],[10,11,16,20],[23,30,34,60]], target = 3
+输出：true
+
+*/
+
+func searchMatrixI(matrix [][]int, target int) bool {
+	for i := 0; i < len(matrix); i++ {
+		if matrix[i][0] <= target && target <= matrix[i][len(matrix[i])-1] {
+			for j := 0; j < len(matrix[i]); j++ {
+				if matrix[i][j] == target {
+					return true
+				}
+			}
+		}
+	}
+	return false
+}
+
+/**
+	21 搜索二维矩阵II
+编写一个高效的算法来搜索 m x n 矩阵 matrix 中的一个目标值 target 。该矩阵具有以下特性：
+	!! 每行的元素从左到右升序排列。
+	每列的元素从上到下升序排列。
+
+	示例：
+		输入：matrix = [[1,4,7,11,15],[2,5,8,12,19],[3,6,9,16,22],[10,13,14,17,24],[18,21,23,26,30]], target = 5
+		输出：true
+
+解法说明： 上searchMatrixI 也可用  时间复杂度 m*n
+
+优化解法：searchMatrixII
+因为矩阵的行和列是排序的（分别从左到右和从上到下），所以在查看任何特定值时，我们可以修剪O(m)O(m)或O(n)O(n)元素。
+
+算法：
+首先，我们初始化一个指向矩阵左下角的 (row，col)(row，col) 指针。然后，直到找到目标并返回 true（或者指针指向矩阵维度之外的 (row，col)(row，col) 为止，
+我们执行以下操作：如果当前指向的值大于目标值，则可以 “向上” 移动一行。 否则，如果当前指向的值小于目标值，则可以移动一列。
+不难理解为什么这样做永远不会删减正确的答案；因为行是从左到右排序的，所以我们知道当前值右侧的每个值都较大。 因此，如果当前值已经大于目标值，
+我们知道它右边的每个值会比较大。也可以对列进行非常类似的论证，因此这种搜索方式将始终在矩阵中找到目标（如果存在）。
+
+时间复杂度：O(n+m)。
+时间复杂度分析的关键是注意到在每次迭代（我们不返回 true）时，行或列都会精确地递减/递增一次。由于行只能减少 mm 次，而列只能增加 nn 次，因此在导致 while 循环终止之前，循环不能运行超过 n+mn+m 次。因为所有其他的工作都是常数，所以总的时间复杂度在矩阵维数之和中是线性的。
+空间复杂度：O(1)，因为这种方法只处理几个指针，所以它的内存占用是恒定的。
+
+*/
+
+func searchMatrixII(matrix [][]int, target int) bool {
+
+	row := len(matrix) - 1
+	col := 0
+	for row >= 0 && col < len(matrix[0]) {
+		if matrix[row][col] > target {
+			row--
+		} else if matrix[row][col] < target {
+			col++
+		} else {
+			return true
+		}
+	}
+	return false
+}
+
 /*
 复杂度分析
 时间复杂度：O(N)O(N)
